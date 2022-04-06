3c3165402feb56d44cf4255187407063c524e6ec
nazaf.rabbi <nazaf.rabbi@BTSL-NazafRabbi.btraccl.com>
2022-04-06 15:40:31 +0600

    added TodoList model

--- toDoWebApp/todoApp/models.py
+++ toDoWebApp/todoApp/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class TodoList(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    task = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
